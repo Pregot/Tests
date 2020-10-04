e891e3ce44d8dcc6e1c0ab9dccf9ac4cb6fe178b
Pregot <72355982+Pregot@users.noreply.github.com>
2020-10-04 21:59:03 +0300

    hdhfdhfdn

--- socialnetwork/src/socialnetworks/Myfriend.java
+++ socialnetwork/src/socialnetworks/Myfriend.java
@@ -5,7 +5,7 @@ import lt.infobalt.itakademija.javalang.exam.socialnetwork.Friend;
 public class Myfriend extends Friend {
 
 	public Myfriend(String firstName, String lastName, String city) {
-		super(firstName, lastName, city);
+		super(firstName, lastName);
 	}
 
 }
