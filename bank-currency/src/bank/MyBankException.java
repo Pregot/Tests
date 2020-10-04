7ebd3b0c9dafae6d79883cdfc75d6c7e8edeb0b6
Pregot <72355982+Pregot@users.noreply.github.com>
2020-10-04 22:09:14 +0300

    fsafas

--- bank-currency/src/bank/MyBankException.java
+++ bank-currency/src/bank/MyBankException.java
@@ -4,7 +4,7 @@ import lt.itakademija.exam.BankException;
 
 public class MyBankException extends BankException{
 
-	public MyBankException(String message) {
+	public MyBankException(Int message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
