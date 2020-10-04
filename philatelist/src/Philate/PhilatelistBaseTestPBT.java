56958a270fbda8ddc54ba71fe084a5f18f9085bf
Pregot <72355982+Pregot@users.noreply.github.com>
2020-10-04 22:07:57 +0300

    ggfgd

--- philatelist/src/Philate/PhilatelistBaseTestPBT.java
+++ philatelist/src/Philate/PhilatelistBaseTestPBT.java
@@ -8,7 +8,6 @@ public class PhilatelistBaseTestPBT extends PhilatelistBaseTest  {
 	@Override
 	protected Philatelist getPhilatelist() {
 		PostStampps pav = new PostStampps();
-		return pav;
 	}
 	
 }
